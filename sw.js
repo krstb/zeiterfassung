fea2bd3a6b1805df90487ab120f98f101ca0ad5c
Robert <krstb-90@yahoo.com>
2026-01-12 18:12:52 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'zeiterfassung-v1'; // Ändere v um Updates zu erzwingen
+const CACHE_NAME = 'zeiterfassung-v2'; // Ändere v um Updates zu erzwingen
 const ASSETS = [
   'index.html',
   'manifest.json',
